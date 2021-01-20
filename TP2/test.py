4acc63d76c4b39ddb379b2202a5e3101eb75a495
daniel-deutsch <danieljorged1@gmail.com>
2021-01-20 17:51:28 +0100

    exercises 1 to 5

---
+++ TP2/test.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
